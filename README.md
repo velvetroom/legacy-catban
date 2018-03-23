6fa07dc126f4e155b523ec2495b55f4ecc6626a1
Zero <zero.griffin@gmail.com>
2018-03-23 08:31:19 +0100

    Add folder structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# catban
+
+![iOS](https://img.shields.io/badge/iOS-9.0%2B-orange.svg)
+![Swift](https://img.shields.io/badge/Swift-4-orange.svg)
+[![Bitrise](https://www.bitrise.io/app/ff6f79b65d3cd3be/status.svg?token=7nMsF83xIOfIaJTGL5e1xQ)](https://www.bitrise.io/app/ff6f79b65d3cd3be)
---
+++ Scripts/versioner.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+build=$(git rev-list HEAD --count)
+version=$(agvtool what-marketing-version -terse1)
+
+if [[ $version == *"."* ]]; then
+
+    version_major_minor=${version%.*}
+    version_major=${version_major_minor%.*}
+
+    if [[ $version_major_minor == *"."* ]]; then
+
+        version_minor=${version_major_minor##*.}
+
+    else
+
+        version_minor="0"
+    fi
+fi
+
+new_version="$version_major.$version_minor.$build"
+
+if [[ $new_version != $version ]]; then
+    sleep 1
+
+    agvtool new-version -all $build
+    agvtool new-marketing-version $new_version
+fi
