9b0fb3e51fff740d7d530ecdbd77984fc35df1dd
Zero <zero.griffin@gmail.com>
2018-03-23 15:47:55 +0100

    Create project card

---
+++ Unit/Model/Project/TestProjectCard.swift
@@ -0,0 +1,14 @@
+import XCTest
+
+class TestProjectCard:XCTestCase {
+    private var card:ProjectCard!
+    
+    override func setUp() {
+        super.setUp()
+        self.card = ProjectCard()
+    }
+    
+    func testLoad() {
+        XCTAssertNotNil(self.card, "Failed to load card")
+    }
+}
