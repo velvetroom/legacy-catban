653db5ceabcf2c5a69a393034151d1c220045340
Zero <zero.griffin@gmail.com>
2018-03-23 19:37:13 +0100

    Load new project on project loader

--- Source/Model/Project/ProjectLoader.swift
+++ Source/Model/Project/ProjectLoader.swift
@@ -2,6 +2,7 @@ import Foundation
 
 class ProjectLoader:ProjectLoaderProtocol {
     func load(completion:@escaping((Project) -> Void)) {
-        
+        let project:Project = Project.factoryNewProject()
+        completion(project)
     }
 }
--- Unit/Model/Project/TestProjectLoader.swift
+++ Unit/Model/Project/TestProjectLoader.swift
@@ -3,6 +3,10 @@ import XCTest
 
 class TestProjectLoader:XCTestCase {
     private var loader:ProjectLoader!
+    private var expect:XCTestExpectation?
+    private struct Constants {
+        static let wait:TimeInterval = 0.3
+    }
     
     override func setUp() {
         super.setUp()
@@ -11,5 +15,23 @@ class TestProjectLoader:XCTestCase {
     
     func testLoad() {
         XCTAssertNotNil(self.loader, "Failed to load loader")
+    }
+    
+    func testLoadProject() {
+        self.startExpectation()
+        
+        self.loader.load { [weak self] (project:Project) in
+            self?.expect?.fulfill()
+        }
+        
+        self.waitExpectations()
+    }
+    
+    private func startExpectation() {
+        self.expect = expectation(description:"Wait for expectation")
+    }
+    
+    private func waitExpectations() {
+        waitForExpectations(timeout:Constants.wait) { (error:Error?) in }
     }
 }
