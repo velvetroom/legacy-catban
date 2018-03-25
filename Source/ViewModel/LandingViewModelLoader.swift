44523a8d08b836f6c57a52373eddbe05c0dc28d7
Zero <zero.griffin@gmail.com>
2018-03-25 23:11:12 +0200

    Hide logo with view model

--- Source/ViewModel/LandingViewModelLoader.swift
+++ Source/ViewModel/LandingViewModelLoader.swift
@@ -1,8 +1,13 @@
 import Foundation
 
 class LandingViewModelLoader:LandingViewModelLoaderProtocol {
+    private struct Constants {
+        static let logoHidden:Bool = true
+    }
+    
     func factoryViewModelWith(project:Project) -> LandingViewModel {
         var viewModel:LandingViewModel = LandingViewModel()
+        viewModel.logoHidden = Constants.logoHidden
         viewModel.collection = self.factoryCollectionWith(project:project)
         return viewModel
     }
