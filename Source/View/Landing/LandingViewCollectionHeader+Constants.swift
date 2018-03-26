ad147fe79ef2907284e60306141b953e40f88950
Zero <zero.griffin@gmail.com>
2018-03-26 10:02:06 +0200

    Configure title view

---
+++ Source/View/Landing/LandingViewCollectionHeader+Constants.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+extension LandingViewCollectionHeader {
+    struct Constants {
+        static let titleFontSize:CGFloat = 13
+        static let titleTop:CGFloat = 12
+    }
+}
--- Source/View/Landing/LandingViewCollectionHeader.swift
+++ Source/View/Landing/LandingViewCollectionHeader.swift
@@ -19,12 +19,13 @@ class LandingViewCollectionHeader:UICollectionReusableView {
         labelTitle.translatesAutoresizingMaskIntoConstraints = false
         labelTitle.isUserInteractionEnabled = false
         labelTitle.textAlignment = NSTextAlignment.center
-        labelTitle.textColor = UIColor(white:0.3, alpha:1)
+        labelTitle.textColor = UIColor(white:0.2, alpha:1)
+        labelTitle.font = UIFont.systemFont(ofSize:Constants.titleFontSize, weight:UIFont.Weight.light)
         self.labelTitle = labelTitle
         
         self.addSubview(labelTitle)
         
-        labelTitle.topAnchor.constraint(equalTo:self.topAnchor).isActive = true
+        labelTitle.topAnchor.constraint(equalTo:self.topAnchor, constant:Constants.titleTop).isActive = true
         labelTitle.heightAnchor.constraint(greaterThanOrEqualToConstant:0).isActive = true
         labelTitle.leftAnchor.constraint(equalTo:self.leftAnchor).isActive = true
         labelTitle.rightAnchor.constraint(equalTo:self.rightAnchor).isActive = true
