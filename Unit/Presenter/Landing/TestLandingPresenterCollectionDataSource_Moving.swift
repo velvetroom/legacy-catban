03f7976eb6b207ef64ffdb8dae7b5274d0c659c0
Zero <zero.griffin@gmail.com>
2018-03-28 08:03:29 +0200

    Test moving item

--- Unit/Presenter/Landing/TestLandingPresenterCollectionDataSource_Moving.swift
+++ Unit/Presenter/Landing/TestLandingPresenterCollectionDataSource_Moving.swift
@@ -3,13 +3,29 @@ import XCTest
 
 class TestLandingPresenterCollectionDataSource_Moving:XCTestCase {
     private var presenter:LandingPresenterCollectionDataSource!
+    private var viewModel:LandingViewModelCollection!
+    private var collection:MockLandingViewCollection!
     
     override func setUp() {
         super.setUp()
         self.presenter = LandingPresenterCollectionDataSource()
+        self.viewModel = LandingViewModelCollection()
+        self.collection = MockLandingViewCollection()
+        self.configureViewModel()
+        self.presenter.viewModel = self.viewModel
     }
     
     func testLoad() {
         XCTAssertNotNil(self.presenter, "Failed to load presenter")
+    }
+    
+    func testMoveItem() {
+        let startingIndex:IndexPath = IndexPath(item:1, section:0)
+        let endingIndex:IndexPath = IndexPath(item:0, section:0)
+        self.presenter.collectionView(self.collection, moveItemAt:startingIndex, to:endingIndex)
+    }
+    
+    private func configureViewModel() {
+        
     }
 }
