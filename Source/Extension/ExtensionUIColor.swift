bd698d7fb5787222225dc4e4365b3afba58ace15
Zero <zero.griffin@gmail.com>
2018-03-29 09:18:43 +0200

    Add shared colours

---
+++ Source/Extension/ExtensionUIColor.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIColor {
+    struct Shared {
+        static let grayColour:UIColor = UIColor(red:0.92, green:0.94, blue:0.96, alpha:1)
+        static let redColour:UIColor = UIColor(red:1, green:0.22, blue:0, alpha:1)
+        static let blueColour:UIColor = UIColor(red:0.3, green:0.65, blue:0.85, alpha:1)
+    }
+}
