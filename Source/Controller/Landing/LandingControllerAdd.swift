7f98df7fa02f6e3d3cbbb8bd9110c5555d17b53d
Zero <zero.griffin@gmail.com>
2018-04-05 07:57:51 +0200

    Add test to landing controller add

--- Source/Controller/Landing/LandingControllerAdd.swift
+++ Source/Controller/Landing/LandingControllerAdd.swift
@@ -1,5 +1,25 @@
 import UIKit
 
 class LandingControllerAdd:UIAlertController {
+    var model:LandingProtocol
     
+    init() {
+        self.model = Landing()
+        super.init(nibName:nil, bundle:nil)
+    }
+    
+    required init?(coder:NSCoder) {
+        return nil
+    }
+    
+    override var preferredStyle:UIAlertControllerStyle {
+        get {
+            return UIAlertControllerStyle.actionSheet
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.factoryActions()
+    }
 }
--- Unit/Controller/Landing/TestLandingControllerAdd.swift
+++ Unit/Controller/Landing/TestLandingControllerAdd.swift
@@ -3,13 +3,35 @@ import XCTest
 
 class TestLandingControllerAdd:XCTestCase {
     private var controller:LandingControllerAdd!
+    private var model:LandingProtocol!
+    private struct Constants {
+        static let expectedActions:Int = 4
+    }
     
     override func setUp() {
         super.setUp()
         self.controller = LandingControllerAdd()
+        self.model = MockLandingProtocol()
+        self.controller.model = self.model
     }
     
     func testLoad() {
         XCTAssertNotNil(self.controller, "Failed to load controller")
+        XCTAssertNotNil(self.model, "Failed to load model")
+    }
+    
+    func testViewDidLoad() {
+        XCTAssertNotNil(self.controller.view, "Failed to load view")
+        XCTAssertGreaterThan(self.controller.actions.count, 0, "Not loading actions on view did load")
+    }
+    
+    func testPreferredStyle() {
+        XCTAssertEqual(self.controller.preferredStyle, UIAlertControllerStyle.actionSheet, "Invalid style")
+    }
+    
+    func testAddActions() {
+        self.controller.factoryActions()
+        let countActions:Int = self.controller.actions.count
+        XCTAssertEqual(countActions, Constants.expectedActions, "Failed to factory actions")
     }
 }
