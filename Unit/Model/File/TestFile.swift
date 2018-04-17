94dd2e7f2e68b5b24961a06f890cd6d756c18902
Zero <zero.griffin@gmail.com>
2018-04-17 10:00:43 +0200

    Create file protocol

---
+++ Unit/Model/File/TestFile.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import catban
+
+class TestFile:XCTestCase {
+    private var model:File!
+    
+    override func setUp() {
+        super.setUp()
+        self.model = File()
+    }
+    
+    func testLoad() {
+        XCTAssertNotNil(self.model, "Failed to load model")
+    }
+}
