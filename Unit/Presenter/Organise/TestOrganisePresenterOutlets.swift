29435495cf3ddbce6084bb7db0e26a99aa192c15
Zero <zero.griffin@gmail.com>
2018-04-20 11:46:54 +0200

    Create outlets

---
+++ Unit/Presenter/Organise/TestOrganisePresenterOutlets.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import catban
+
+class TestOrganisePresenterOutlets:XCTestCase {
+    private var outlets:OrganisePresenterOutlets!
+    
+    override func setUp() {
+        super.setUp()
+        self.outlets = OrganisePresenterOutlets()
+    }
+    
+    func testLoad() {
+        XCTAssertNotNil(self.outlets, "Failed to load outlets")
+    }
+}
