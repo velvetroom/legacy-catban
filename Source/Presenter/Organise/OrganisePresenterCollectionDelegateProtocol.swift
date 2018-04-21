501d3fae7b4dddfbaf05413ca07a5cc918876113
Zero <zero.griffin@gmail.com>
2018-04-21 16:41:48 +0200

    Remove redundant conformance

--- Source/Presenter/Organise/OrganisePresenterCollectionDelegateProtocol.swift
+++ Source/Presenter/Organise/OrganisePresenterCollectionDelegateProtocol.swift
@@ -1,5 +1,5 @@
 import UIKit
 
-protocol OrganisePresenterCollectionDelegateProtocol:UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+protocol OrganisePresenterCollectionDelegateProtocol:UICollectionViewDelegateFlowLayout {
     
 }
