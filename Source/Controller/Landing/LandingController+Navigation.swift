329f49585d6e4f76e41632f9aec1c163ea61025e
Zero <zero.griffin@gmail.com>
2018-04-21 16:38:08 +0200

    Update navigation items

--- Source/Controller/Landing/LandingController+Navigation.swift
+++ Source/Controller/Landing/LandingController+Navigation.swift
@@ -1,14 +1,29 @@
 import UIKit
 
 extension LandingController {
+    private var rightBarButtonItems:[UIBarButtonItem] {
+        get {
+            return [UIBarButtonItem(barButtonSystemItem:UIBarButtonSystemItem.organize, target:self,
+                                    action:#selector(self.selectorOrganise(sender:))),
+                    UIBarButtonItem(barButtonSystemItem:UIBarButtonSystemItem.compose, target:self,
+                                    action:#selector(self.selectorOrganise(sender:))),
+                    UIBarButtonItem(title:"Stats", style:UIBarButtonItemStyle.plain, target:self,
+                                    action:#selector(self.selectorOrganise(sender:))),
+                    UIBarButtonItem(barButtonSystemItem:UIBarButtonSystemItem.add, target:self,
+                                    action:#selector(self.selectorAdd(sender:)))]
+        }
+    }
+    
+    private var leftBarButtonItems:[UIBarButtonItem] {
+        get {
+            return []
+        }
+    }
+    
     func adjustNavigationItem() {
         self.navigationItem.largeTitleDisplayMode = UINavigationItem.LargeTitleDisplayMode.always
-        self.navigationItem.rightBarButtonItems = [UIBarButtonItem(barButtonSystemItem:UIBarButtonSystemItem.add,
-            target:self,action:#selector(self.selectorAdd(sender:))),UIBarButtonItem(title:"Stats", style:UIBarButtonItemStyle.plain, target:self, action:#selector(self.selectorOrganise(sender:)))]
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem:UIBarButtonSystemItem.organize,
-            target:self,
-            action:#selector(self.selectorOrganise(sender:)))
+        self.navigationItem.rightBarButtonItems = self.rightBarButtonItems
+        self.navigationItem.leftBarButtonItems = self.leftBarButtonItems
         self.title = String.localizedLanding(key:"LandingController_title")
     }
 }
