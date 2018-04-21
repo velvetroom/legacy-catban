d9e6894df6e367c30ea1774371692e8c8eac6c43
Zero <zero.griffin@gmail.com>
2018-04-21 11:32:32 +0200

    Factory view model

--- Source/Presenter/Organise/OrganisePresenterFactory.swift
+++ Source/Presenter/Organise/OrganisePresenterFactory.swift
@@ -2,6 +2,23 @@ import Foundation
 
 class OrganisePresenterFactory:OrganisePresenterFactoryProtocol {
     func factoryWith(board:BoardProtocol) -> OrganiseViewModel {
-        return OrganiseViewModel()
+        var viewModel:OrganiseViewModel = OrganiseViewModel()
+        viewModel.collection = self.factoryCollectionWith(board:board)
+        return viewModel
+    }
+    
+    private func factoryCollectionWith(board:BoardProtocol) -> OrganiseViewModelCollection {
+        var viewModel:OrganiseViewModelCollection = OrganiseViewModelCollection()
+        for project:ProjectProtocol in board.projects {
+            let item:OrganiseViewModelCollectionItem = self.factoryCollectionItemWith(project:project)
+            viewModel.items.append(item)
+        }
+        return viewModel
+    }
+    
+    private func factoryCollectionItemWith(project:ProjectProtocol) -> OrganiseViewModelCollectionItem {
+        var viewModel:OrganiseViewModelCollectionItem = OrganiseViewModelCollectionItem()
+        viewModel.name = project.name
+        return viewModel
     }
 }
