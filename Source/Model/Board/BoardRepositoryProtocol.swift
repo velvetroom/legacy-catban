3bd5fc16ed6cf890486d60292ff90928a57d59bb
Zero <zero.griffin@gmail.com>
2018-04-28 12:43:33 +0200

    Refactor board repository

--- Source/Model/Board/BoardRepositoryProtocol.swift
+++ Source/Model/Board/BoardRepositoryProtocol.swift
@@ -1,8 +1,5 @@
 import Foundation
 
 protocol BoardRepositoryProtocol {
-    var repository:RepositoryProtocol { get set }
-    var dispatchQueue:DispatchQueue { get set }
-    
     func save(board:BoardProtocol)
 }
