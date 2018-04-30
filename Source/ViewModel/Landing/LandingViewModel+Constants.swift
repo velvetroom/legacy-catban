2b03ff40b6710982324489ccb154e5614f6d34fe
Zero <zero.griffin@gmail.com>
2018-04-30 16:06:31 +0200

    Refactor collection view height

--- Source/ViewModel/Landing/LandingViewModel+Constants.swift
+++ Source/ViewModel/Landing/LandingViewModel+Constants.swift
@@ -2,8 +2,18 @@ import UIKit
 
 extension LandingViewModel {
     struct Constants {
-        static let collectionMenuHeight:CGFloat = 45
         static let collectionMenuAnimationDuration:TimeInterval = 0.3
         static let waitBeforeScrolling:TimeInterval = 0.1
+        private static let baseCollectionMenuHeight:CGFloat = 45
+        
+        static var collectionMenuHeight:CGFloat {
+            get {
+                var height:CGFloat = baseCollectionMenuHeight
+                if let safeAreaBottom:CGFloat = UIApplication.shared.keyWindow?.safeAreaInsets.bottom {
+                    height += safeAreaBottom
+                }
+                return height
+            }
+        }
     }
 }
