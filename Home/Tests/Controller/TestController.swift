a742efe5ac95fd61d6f3f03b9a46a6d531069aaf
Zero <zero.griffin@gmail.com>
2018-05-03 10:01:22 +0200

    Create controller

---
+++ Home/Tests/Controller/TestController.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import Home
+
+class TestController:XCTestCase {
+    private var controller:Controller!
+    
+    override func setUp() {
+        super.setUp()
+        self.controller = Controller()
+    }
+    
+    func testLoad() {
+        
+    }
+}
