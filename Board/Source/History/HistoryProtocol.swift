11ba49629862742150509c03649156c009d38afa
Zero <zero.griffin@gmail.com>
2018-05-03 09:46:18 +0200

    Reorganise tests

--- Board/Source/History/HistoryProtocol.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol HistoryProtocol {
-    var items:[HistoryItemProtocol] { get set }
-    
-    func created(card:ProjectCard)
-    func moved(card:ProjectCard, to column:ProjectColumn)
-}
--- Board/Source/Project/ProjectColumn+Factory.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension ProjectColumn {
-    class func factoryColumn() -> ProjectColumn {
-        let column:ProjectColumn = ProjectColumn()
-        column.identifier = UUID().uuidString
-        return column
-    }
-}
