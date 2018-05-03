bb0217879fb58966a0ba39165366a17f64a93638
Zero <zero.griffin@gmail.com>
2018-05-03 08:26:53 +0200

    Create Board

---
+++ Board/Tests/TestBoard.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import Board
+
+class TestBoard:XCTestCase {
+    private var model:Board!
+    
+    override func setUp() {
+        super.setUp()
+        self.model = Board()
+    }
+    
+    func testLoad() {
+        
+    }
+}
