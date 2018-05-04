7d39d295e30aa91b774553f65e33ed10a1263c15
Zero <zero.griffin@gmail.com>
2018-05-04 07:49:52 +0200

    Add logo to load view

--- Load/Source/View/View.swift
+++ Load/Source/View/View.swift
@@ -2,12 +2,24 @@ import UIKit
 import Shared
 
 class View:Shared.View {
-    override init() {
-        super.init()
-        self.view.backgroundColor = UIColor.blue
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.addLogo()
     }
     
-    required init?(coder:NSCoder) {
-        return nil
+    private func addLogo() {
+        let imageLogo:UIImageView = UIImageView()
+        imageLogo.translatesAutoresizingMaskIntoConstraints = false
+        imageLogo.clipsToBounds = true
+        imageLogo.isUserInteractionEnabled = false
+        imageLogo.contentMode = UIViewContentMode.center
+        imageLogo.image = UIImage(name:ViewConstants.assetLogo, in:type(of:self))
+        
+        self.view.addSubview(imageLogo)
+        
+        imageLogo.topAnchor.constraint(equalTo:self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        imageLogo.bottomAnchor.constraint(equalTo:self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        imageLogo.leftAnchor.constraint(equalTo:self.view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        imageLogo.rightAnchor.constraint(equalTo:self.view.safeAreaLayoutGuide.rightAnchor).isActive = true
     }
 }
--- Shared/Source/UIImage+Bundle.swift
+++ Shared/Source/UIImage+Bundle.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-extension UIImage {
+public extension UIImage {
     convenience init?(name:String, in type:AnyClass) {
         let bundle:Bundle = Bundle(for:type)
         self.init(named:name, in:bundle, compatibleWith:nil)
