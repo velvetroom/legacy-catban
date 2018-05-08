3732fcd040153ccde9916d45756783b5bf1c626d
Zero <zero.griffin@gmail.com>
2018-05-08 12:13:46 +0200

    Load view board

--- Home/Source/Presenter/PresenterOutletsLoader.swift
+++ Home/Source/Presenter/PresenterOutletsLoader.swift
@@ -8,6 +8,17 @@ class PresenterOutletsLoader {
     }
     
     func loadFor(view:UIView) {
-        
+        self.loadScroll(view:view)
+        self.loadBoard()
+    }
+    
+    private func loadScroll(view:UIView) {
+        self.outlets.viewScroll = view as? ViewScroll
+    }
+    
+    private func loadBoard() {
+        let viewBoard:ViewBoard = ViewBoard()
+        self.outlets.viewScroll?.addSubview(viewBoard)
+        self.outlets.viewBoard = viewBoard
     }
 }
