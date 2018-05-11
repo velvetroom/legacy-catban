6641316a70cc007b458996781b1e95e56f272c62
Zero <zero.griffin@gmail.com>
2018-05-11 14:43:36 +0200

    Test card content is being updated

--- Card/Tests/View/TestView.swift
+++ Card/Tests/View/TestView.swift
@@ -1,16 +1,28 @@
 import XCTest
+import Board
 @testable import Card
 @testable import Shared
 
 class TestView:XCTestCase {
     private var view:Card.View!
     private var controller:MockController!
+    private var card:CardProtocol!
+    private var textView:UITextView!
+    private struct Constants {
+        static let initialText:String = "hello world"
+        static let updatedText:String = "lorem ipsum"
+    }
     
     override func setUp() {
         super.setUp()
         self.view = Card.View()
         self.controller = MockController()
+        self.card = CardFactory.newCard()
+        self.textView = UITextView()
         self.view.controller = self.controller
+        self.controller.card = self.card
+        self.card.content = Constants.initialText
+        self.textView.text = Constants.updatedText
     }
     
     func testTrashImage() {
@@ -31,5 +43,10 @@ class TestView:XCTestCase {
         
         self.view.selectorDone(sender:UIBarButtonItem())
         XCTAssertTrue(calledController, "Not called")
+    }
+    
+    func testUpdateCardContent() {
+        self.view.textViewDidChange(self.textView)
+        XCTAssertEqual(self.card.content, Constants.updatedText, "Failed to update card")
     }
 }
