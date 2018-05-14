47d86e6f6ae0fc1cc3b5089e18c084fb01582cf8
Zero <zero.griffin@gmail.com>
2018-05-14 12:09:38 +0200

    Assign delegate to drag

--- Home/Tests/Presenter/TestPresenterOutletsLoader.swift
+++ Home/Tests/Presenter/TestPresenterOutletsLoader.swift
@@ -3,31 +3,26 @@ import XCTest
 
 class TestPresenterOutletsLoader:XCTestCase {
     private var presenter:PresenterOutletsLoader!
-    private var parentPresenter:Presenter!
-    private var outlets:PresenterOutlets!
-    private var view:View!
     
     override func setUp() {
         super.setUp()
         self.presenter = PresenterOutletsLoader()
-        self.view = View()
-        self.parentPresenter = Presenter()
-        self.presenter.presenter = self.parentPresenter
-        self.presenter.view = self.view
-        self.presenter.loadOutlets()
-        self.outlets = self.parentPresenter.outlets
-    }
-    
-    func testLoadOutlets() {
-        XCTAssertNotNil(self.outlets.view, "Failed to load")
-        XCTAssertNotNil(self.outlets.viewScroll, "Failed to load")
-        XCTAssertNotNil(self.outlets.viewBoard, "Failed to load")
-        XCTAssertNotNil(self.outlets.viewBoard?.dragDelegate, "Delegate not assigned")
     }
     
     func testNotRetainingView() {
-        self.parentPresenter = nil
         self.presenter.view = View()
         XCTAssertNil(self.presenter.view, "Retaining view")
+    }
+    
+    func testLoadOutlets() {
+        let view:View = View()
+        let parentPresenter:Presenter = Presenter()
+        self.presenter.presenter = parentPresenter
+        self.presenter.view = view
+        self.presenter.loadOutlets()
+        XCTAssertNotNil(parentPresenter.outlets.view, "Failed to load")
+        XCTAssertNotNil(parentPresenter.outlets.viewScroll, "Failed to load")
+        XCTAssertNotNil(parentPresenter.outlets.viewBoard, "Failed to load")
+        XCTAssertNotNil(parentPresenter.outlets.viewBoard?.dragDelegate, "Delegate not assigned")
     }
 }
