94b6914d77e1fe9b17351c336bd1d9be439c4638
Zero <zero.griffin@gmail.com>
2018-05-14 13:47:48 +0200

    Use gesture states

--- Home/Source/Presenter/PresenterBoardDrag.swift
+++ Home/Source/Presenter/PresenterBoardDrag.swift
@@ -1,7 +1,28 @@
 import UIKit
 
 class PresenterBoardDrag:PresenterBoardDragProtocol {
+    private var router:[PresenterBoardDragState : ((CGPoint) -> Void)] {
+        get {
+            return [
+                PresenterBoardDragState.began : self.stateBegan,
+                PresenterBoardDragState.changed : self.stateChanged,
+                PresenterBoardDragState.ended : self.stateEnded]
+        }
+    }
+    
     func updated(state:PresenterBoardDragState, at position:CGPoint) {
+        self.router[state]!(position)
+    }
+    
+    private func stateBegan(position:CGPoint) {
+        
+    }
+    
+    private func stateChanged(position:CGPoint) {
+        
+    }
+    
+    private func stateEnded(position:CGPoint) {
         
     }
 }
--- Home/Tests/Presenter/TestPresenterBoardDrag.swift
+++ Home/Tests/Presenter/TestPresenterBoardDrag.swift
@@ -3,11 +3,24 @@ import XCTest
 
 class TestPresenterBoardDrag:XCTestCase {
     private var presenter:PresenterBoardDrag!
+    private struct Constants {
+        static let poisition:CGPoint = CGPoint(x:3, y:4)
+    }
     
     override func setUp() {
         super.setUp()
         self.presenter = PresenterBoardDrag()
     }
     
+    func testBegan() {
+        self.presenter.updated(state:PresenterBoardDragState.began, at:Constants.poisition)
+    }
     
+    func testChanged() {
+        self.presenter.updated(state:PresenterBoardDragState.changed, at:Constants.poisition)
+    }
+    
+    func testEnded() {
+        self.presenter.updated(state:PresenterBoardDragState.ended, at:Constants.poisition)
+    }
 }
