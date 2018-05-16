43ad1ca1e6a55e2f7a565fdb8cbe14a593cd6e89
Zero <zero.griffin@gmail.com>
2018-05-16 14:30:39 +0200

    Create drag item protocol

--- Home/Source/Drag/DragItemProtocol.swift
+++ Home/Source/Drag/DragItemProtocol.swift
@@ -1,12 +1,5 @@
 import UIKit
 
-protocol DragItemProtocol:class {
+protocol DragItemProtocol:DragObjectProtocol {
     var parent:DragParentProtocol! { get set }
-    var view:ViewBoardItem! { get set }
-    var minX:CGFloat { get }
-    var minY:CGFloat { get }
-    var midX:CGFloat { get }
-    var midY:CGFloat { get }
-    var maxX:CGFloat { get }
-    var maxY:CGFloat { get }
 }
