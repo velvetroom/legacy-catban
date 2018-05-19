593bae504e7b9216a0f17f2120c8bf92f8455b53
Zero <zero.griffin@gmail.com>
2018-05-19 13:10:21 +0200

    Clean up view

--- Home/Source/View/ViewBoardElement.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-class ViewBoardElement:UIView {
-    weak var layoutLeft:NSLayoutConstraint!
-    weak var layoutTop:NSLayoutConstraint!
-    weak var layoutWidth:NSLayoutConstraint!
-    weak var layoutHeight:NSLayoutConstraint!
-}
