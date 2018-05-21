0d4acea4ba68732fc358d50d049277525ef04631
Zero <zero.griffin@gmail.com>
2018-05-21 10:32:34 +0200

    Test actions triggered

--- Canvas/Tests/View/TestViewCard.swift
+++ Canvas/Tests/View/TestViewCard.swift
@@ -13,4 +13,15 @@ class TestViewCard:XCTestCase {
         let stateType:DragStateDynamic.Type? = self.view.dragState as? DragStateDynamic.Type
         XCTAssertNotNil(stateType, "Invalid state type")
     }
+    
+    func testTriggerAction() {
+        var called:Bool = false
+        let canvas:MockCanvasEditorProtocol = MockCanvasEditorProtocol()
+        canvas.onEditCardWithIdentifier = {
+            called = true
+        }
+        
+        self.view.triggerAction(canvas:canvas)
+        XCTAssertTrue(called, "Not called")
+    }
 }
--- Canvas/Tests/View/TestViewNewCard.swift
+++ Canvas/Tests/View/TestViewNewCard.swift
@@ -13,4 +13,15 @@ class TestViewNewCard:XCTestCase {
         let stateType:DragStateFixed.Type? = self.view.dragState as? DragStateFixed.Type
         XCTAssertNotNil(stateType, "Invalid state type")
     }
+    
+    func testTriggerAction() {
+        var called:Bool = false
+        let canvas:MockCanvasEditorProtocol = MockCanvasEditorProtocol()
+        canvas.onCreateNewCard = {
+            called = true
+        }
+        
+        self.view.triggerAction(canvas:canvas)
+        XCTAssertTrue(called, "Not called")
+    }
 }
