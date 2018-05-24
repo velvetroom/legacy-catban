3af6454bd9e10d858e3f3449d0db22b320f6f8fe
Zero <zero.griffin@gmail.com>
2018-05-24 07:56:34 +0200

    Add menu assets

--- Home/Source/View/View.swift
+++ Home/Source/View/View.swift
@@ -2,5 +2,19 @@ import UIKit
 import Shared
 
 class View:Shared.View {
+    override func didLoad() {
+        super.didLoad()
+        self.configureNavigationItem()
+    }
     
+    private func configureNavigationItem() {
+        let buttonMore:UIBarButtonItem = UIBarButtonItem(barButtonSystemItem:UIBarButtonSystemItem.add,
+                                                         target:self,
+                                                         action:#selector(self.selectorDone(sender:)))
+        self.navigationItem.rightBarButtonItems = [buttonMore]
+    }
+    
+    @objc func selectorDone(sender button:UIBarButtonItem) {
+        
+    }
 }
