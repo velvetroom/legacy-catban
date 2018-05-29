67a0726b3af937b5b10aef5899548d496c1f0415
Zero <zero.griffin@gmail.com>
2018-05-29 13:29:44 +0200

    Create view list selector

---
+++ Projects/Source/View/ViewListSelector.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class ViewListSelector:UIView {
+    init() {
+        super.init(frame:CGRect.zero)
+        self.configureView()
+    }
+    
+    required init?(coder:NSCoder) {
+        return nil
+    }
+    
+    private func configureView() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.clipsToBounds = true
+        self.contentMode = UIViewContentMode.center
+        self.isUserInteractionEnabled = false
+    }
+}
