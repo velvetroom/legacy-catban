559bf954cddb8da08eb9a4b03a1adb5620faa1e4
Zero <zero.griffin@gmail.com>
2018-05-30 16:59:34 +0200

    Create test for renamer

---
+++ Projects/Tests/Presenter/TestPresenter_Renamer.swift
@@ -0,0 +1,18 @@
+import XCTest
+@testable import Project
+
+class TestPresenter_Renamer:XCTestCase {
+    private var presenter:Presenter!
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testRenameProjectShowsCurrentName() {
+        XCTAssertTrue(false)
+    }
+    
+    func testUpdateProjectSetsNewName() {
+        XCTAssertTrue(false)
+    }
+}
