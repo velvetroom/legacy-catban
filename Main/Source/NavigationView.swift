f46def6a0084808ee8a35ab84a2d0a77958cb065
Zero <zero.griffin@gmail.com>
2018-06-06 08:19:29 +0200

    Add toolbar to navigation controller

--- Main/Source/NavigationView.swift
+++ Main/Source/NavigationView.swift
@@ -11,11 +11,23 @@ class NavigationView:UINavigationController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureNavigationBar()
+        self.configureToolbar()
+    }
+    
+    private func configureNavigationBar() {
         self.navigationBar.backgroundColor = UIColor.white
         self.navigationBar.barTintColor = UIColor.white
         self.navigationBar.tintColor = UIColor.black
         self.navigationBar.prefersLargeTitles = true
         self.navigationBar.isTranslucent = false
         self.navigationItem.largeTitleDisplayMode = UINavigationItem.LargeTitleDisplayMode.always
+    }
+    
+    private func configureToolbar() {
+        self.toolbar.backgroundColor = UIColor.white
+        self.toolbar.barTintColor = UIColor.white
+        self.toolbar.tintColor = UIColor.black
+        self.toolbar.isTranslucent = false
     }
 }
