97703969f94165ad492496e4c1e8931403937e3c
Zero <zero.griffin@gmail.com>
2018-06-07 09:24:16 +0200

    Parametrise bars in view controllers

--- Shared/Source/Architecture/View.swift
+++ Shared/Source/Architecture/View.swift
@@ -2,8 +2,12 @@ import UIKit
 
 open class View:UIViewController, ViewProtocol {
     open weak var delegate:ViewDelegateProtocol?
+    open var toolbarHidden:Bool
+    open var navigationbarHidden:Bool
     
     public required init() {
+        self.toolbarHidden = true
+        self.navigationbarHidden = true
         super.init(nibName:nil, bundle:nil)
     }
     
@@ -20,6 +24,8 @@ open class View:UIViewController, ViewProtocol {
     
     open override func viewDidAppear(_ animated:Bool) {
         super.viewDidAppear(animated)
+        self.navigationController?.setToolbarHidden(self.toolbarHidden, animated:true)
+        self.navigationController?.setNavigationBarHidden(self.navigationbarHidden, animated:true)
         self.delegate?.didAppear(view:self)
     }
     
