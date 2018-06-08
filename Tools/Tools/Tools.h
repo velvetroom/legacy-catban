22851d22a0699df68b6eb61169a581f5a9562d6f
Zero <zero.griffin@gmail.com>
2018-06-08 07:41:15 +0200

    Create tools module

---
+++ Tools/Tools/Tools.h
@@ -0,0 +1,19 @@
+//
+//  Tools.h
+//  Tools
+//
+//  Created by zero on 08.06.18.
+//  Copyright © 2018 iturbide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tools.
+FOUNDATION_EXPORT double ToolsVersionNumber;
+
+//! Project version string for Tools.
+FOUNDATION_EXPORT const unsigned char ToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tools/PublicHeader.h>
+
+
