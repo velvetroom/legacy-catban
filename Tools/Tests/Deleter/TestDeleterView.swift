dac8153e3958465852a08dc743f28067d3c24126
Zero <zero.griffin@gmail.com>
2018-06-22 11:59:23 +0200

    Test presenter called

--- Tools/Tests/Deleter/TestDeleterView.swift
+++ Tools/Tests/Deleter/TestDeleterView.swift
@@ -5,6 +5,7 @@ class TestDeleterView:XCTestCase {
     private var interactor:MockDeleterInteractorProtocol!
     private var deleter:DeleterView<MockDeleterInteractorProtocol>!
     private var presenter:MockDeleterPresenter!
+    private var transition:MockTransitionProtocol!
     
     override func setUp() {
         super.setUp()
@@ -12,14 +13,34 @@ class TestDeleterView:XCTestCase {
         self.deleter = DeleterFactory.makeWith(
             interactor:self.interactor, and:DeleterViewModel()) as! DeleterView<MockDeleterInteractorProtocol>
         self.presenter = MockDeleterPresenter()
+        self.transition = MockTransitionProtocol()
         self.deleter.presenter = self.presenter
+        self.deleter.transition = self.transition
     }
     
     func testCancelCloseDeleter() {
-        
+        let expect:XCTestExpectation = self.expectation(description:String())
+        var closed:Bool = false
+        self.transition.onDimiss = {
+            closed = true
+            expect.fulfill()
+        }
+        self.deleter.selectorCancel(button:UIButton())
+        self.waitForExpectations(timeout:0.5) { (error:Error?) in
+            XCTAssertTrue(closed, "Failed to close")
+        }
     }
     
     func testCancelCallsPresenter() {
-        
+        let expect:XCTestExpectation = self.expectation(description:String())
+        var closed:Bool = false
+        self.presenter.onCancel = {
+            closed = true
+            expect.fulfill()
+        }
+        self.deleter.selectorCancel(button:UIButton())
+        self.waitForExpectations(timeout:0.5) { (error:Error?) in
+            XCTAssertTrue(closed, "Failed to close")
+        }
     }
 }
