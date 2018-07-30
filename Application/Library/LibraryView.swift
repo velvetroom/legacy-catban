0ce1a2fcc17073d374cfac7f379beedf5ea18d33
Zero <zero.griffin@gmail.com>
2018-07-30 08:16:43 +0200

    Add loading indicator

--- Application/Library/LibraryView.swift
+++ Application/Library/LibraryView.swift
@@ -2,5 +2,23 @@ import Foundation
 import CleanArchitecture
 
 class LibraryView:View<LibraryPresenter> {
+    weak var loading:LoadingView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.makeOutlets()
+        self.layoutOutlets()
+    }
+    
+    private func makeOutlets() {
+        let loading:LoadingView = LoadingView()
+        loading.tintColor = Colors.navyBlue
+        self.loading = loading
+        self.view.addSubview(loading)
+    }
+    
+    private func layoutOutlets() {
+        self.loading.centerXAnchor.constraint(equalTo:self.view.centerXAnchor).isActive = true
+        self.loading.centerYAnchor.constraint(equalTo:self.view.centerYAnchor).isActive = true
+    }
 }
